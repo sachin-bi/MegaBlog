053685161d83d4e279991b5fb2ae5a8f559e2df8
Sachinandan <sachinandanp5@gmail.com>
2024-08-05 17:05:59 +0530

    config file is ready to make sure that its passes string values only

---
+++ src/conf/conf.js
@@ -0,0 +1,16 @@
+//this file can also be named as config variables or environment import, 
+
+//we do this to make it sure that the .env variables should be treated as string (not num or int)
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID),
+}
+
+
+
+
+
+export default conf
